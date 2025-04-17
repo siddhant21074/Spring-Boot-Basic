c4c5878bb5c61fb700e1a9ac3f9245ef116596bd
Siddhant Thombare <165277491+siddhant21074@users.noreply.github.com>
2025-04-17 14:09:21 +0530

    Add files via upload

---
+++ Cal_Assignment6/springboot_project6/src/main/java/com/asign6/springboot_project6/SpringbootProject6Application.java
@@ -0,0 +1,13 @@
+package com.asign6.springboot_project6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProject6Application.class, args);
+		System.out.println("Hello Siddhant");
+	}
+}
---
+++ Cal_Assignment6/springboot_project6/src/test/java/com/asign6/springboot_project6/SpringbootProject6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asign6.springboot_project6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProject6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
